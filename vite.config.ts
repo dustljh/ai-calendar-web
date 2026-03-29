7e87fd123fc5ce4b825006c83b6988e21b66ab6f
dustljh <ij66724@gmail.com>
2026-03-29 23:45:52 +0900

    hosting

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/ai-calendar-web/", // 🔥 추가
+
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
